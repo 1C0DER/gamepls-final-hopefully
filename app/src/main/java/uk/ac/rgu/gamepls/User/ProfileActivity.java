c3dd6043d20a4092edca5f7c2ccc0926432ac01c
1C0DER <106778213+1C0DER@users.noreply.github.com>
2025-03-15 02:06:37 +0000

    back button added to profile page

--- app/src/main/java/uk/ac/rgu/gamepls/User/ProfileActivity.java
+++ app/src/main/java/uk/ac/rgu/gamepls/User/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,12 +17,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import uk.ac.rgu.gamepls.MainActivity;
 import uk.ac.rgu.gamepls.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
     TextView profileName, profileEmail, profileUsername;
     Button editProfile;
+
+    ImageButton backArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +35,18 @@ public class ProfileActivity extends AppCompatActivity {
         profileName = findViewById(R.id.profileName);
         profileEmail = findViewById(R.id.profileEmail);
         profileUsername = findViewById(R.id.profileUsername);
+        backArrow = findViewById(R.id.backArrow);
 //        editProfile = findViewById(R.id.editButton);
 
         showAllUserData();
+
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void showAllUserData(){

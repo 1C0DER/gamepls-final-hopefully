83c236240acdb4a4de0784e7b000bfdf5aa2efb2
1C0DER <106778213+1C0DER@users.noreply.github.com>
2025-04-28 03:51:48 +0100

    splash complete

--- app/src/main/java/uk/ac/rgu/gamepls/SplashActivity.java
+++ app/src/main/java/uk/ac/rgu/gamepls/SplashActivity.java
@@ -3,25 +3,28 @@ package uk.ac.rgu.gamepls;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
+import uk.ac.rgu.gamepls.R;
+import uk.ac.rgu.gamepls.User.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash); // Make sure you have a splash layout
 
-        // Display splash screen for 2 seconds (you can adjust the time as needed)
+        // Delay the transition to the LoginActivity
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // After the delay, start the MainActivity
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                // Start LoginActivity after the delay
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
-                finish();  // Close the SplashActivity
+
+                // Finish SplashActivity so it doesn't stay in the back stack
+                finish();
             }
-        }, 2000);  // 2000 milliseconds = 2 seconds
+        }, 2000); // 2000 ms = 2 seconds delay, adjust this time as needed
     }
 }
